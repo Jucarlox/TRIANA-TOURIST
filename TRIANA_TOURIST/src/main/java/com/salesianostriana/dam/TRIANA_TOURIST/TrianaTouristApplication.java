97c350e18c44378d352359164bc1db2297a9f912
ardana.mujua21 <cardamu02@gmail.com>
2021-12-17 08:19:24 +0100

    Creacion de proyecto spring

---
+++ TRIANA_TOURIST/src/main/java/com/salesianostriana/dam/TRIANA_TOURIST/TrianaTouristApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.TRIANA_TOURIST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrianaTouristApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrianaTouristApplication.class, args);
+	}
+
+}
---
+++ TRIANA_TOURIST/src/test/java/com/salesianostriana/dam/TRIANA_TOURIST/TrianaTouristApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.TRIANA_TOURIST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrianaTouristApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
